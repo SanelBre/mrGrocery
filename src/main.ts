b432d95949b9f0d9d08ede63755cc54ab4da3d61
Sanel Hadzini <sanel.hadzini@gmail.com>
2021-06-15 00:34:11 +0200

    server ready to listen

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
+import "express-async-errors";
+import express from "express";
+import { json } from "body-parser";
+
 import { startDb } from "./utils/db";
 import { seed } from "./seeds";
+import { Log } from "./utils/log";
+
+const PORT = process.env.PORT ?? 8000;
+
+const app = express();
+
+app.use(json());
+
+app.all("*", async () => {
+  throw new Error("Route not found");
+});
 
 startDb().then(async () => {
   await seed();
+  app.listen(PORT, () => Log(`The app is listening on port: ${PORT}`));
 });
